1e1320e339208dca3141399ed6920aa0f970b40f
João Paulo Lima <jptick@gmail.com>
2021-01-28 11:33:11 -0300

    Init Backend Project

---
+++ backend/src/main/java/io/github/joaopaulu/dicionarioanalogico/DicionarioanalogicoApplication.java
@@ -0,0 +1,13 @@
+package io.github.joaopaulu.dicionarioanalogico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DicionarioanalogicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DicionarioanalogicoApplication.class, args);
+	}
+
+}
