97eb81d9029293752a5d2b8abe3b948ceb5173bb
João Paulo <jptick@gmail.com>
2021-04-14 20:37:08 -0300

    Update Layout Frontend

--- front-web/src/core/components/Navbar/index.tsx
+++ front-web/src/core/components/Navbar/index.tsx
@@ -1,30 +1,43 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import {Link, NavLink} from 'react-router-dom';
 import './styles.scss';
 
 const Navbar = () => (
-  <nav className="row bg-primary main-nav">
-    <div className="col-2">
-      <Link to="/" className="nav-logo-text">
-        <h4>Dicionário Online Analógico</h4>
-      </Link>
-    </div>
-    <div className="col-6 offset-2">
-      <ul className="main-menu">
-        <li>
-          <NavLink to="/" exact>
-            HOME
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/products">CATÁLOGO</NavLink>
-        </li>
-        <li>
-          <NavLink to="/admin">ADMIN</NavLink>
-        </li>
-      </ul>
-    </div>
-  </nav>
+    <nav className="row bg-primary main-nav">
+        <div className="col-3">
+            <Link to="/" className="nav-logo-text">
+                <h4>Dicionário Online Analógico</h4>
+            </Link>
+        </div>
+        <div className="col-9">
+            <ul className="main-menu">
+                <li>
+                    <NavLink to="/" exact>INÍCIO</NavLink>
+                </li>
+                <li>
+                    <NavLink to="#">APRESENTAÇÃO</NavLink>
+                </li>
+                <li>
+                    <NavLink to="#">ELABORAÇÃO</NavLink>
+                </li>
+                <li>
+                    <NavLink to="#">GUIA DE CONSULTAS</NavLink>
+                </li>
+                <li>
+                    <NavLink to="#">LISTA DE ABREVIATURAS</NavLink>
+                </li>
+                <li>
+                    <NavLink to="#">CRÉDITOS</NavLink>
+                </li>
+                <li>
+                    <NavLink to="#">REFERÊNCIAS</NavLink>
+                </li>
+                <li>
+                    <NavLink to="#">CONTATO</NavLink>
+                </li>
+            </ul>
+        </div>
+    </nav>
 );
 
 export default Navbar;
