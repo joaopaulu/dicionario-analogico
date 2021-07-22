abae165ffd1a40d49118b5de07543629eace7ebe
João Paulo <jptick@gmail.com>
2021-07-22 19:11:01 -0300

    Create Enum TipoDependencia

---
+++ backend/src/main/java/br/com/dicionarioanalogico/entities/enums/TipoDependenciaCamposTematicos.java
@@ -0,0 +1,9 @@
+package br.com.dicionarioanalogico.entities.enums;
+
+public enum TipoDependenciaCamposTematicos {
+    hip,
+    mer,
+    conProfissional,
+    con,
+    Verbo
+}
---
+++ backend/src/main/java/br/com/dicionarioanalogico/entities/enums/TipoDependenciaVerbetes.java
@@ -0,0 +1,29 @@
+package br.com.dicionarioanalogico.entities.enums;
+
+public enum TipoDependenciaVerbetes {
+     hip,
+     mer,
+     con1,
+     con2,
+     con3,
+     con4,
+     con5,
+     con6,
+     Verbo,
+     sin,
+     con7,
+     con8,
+     con9,
+     con10,
+     con11,
+     con12,
+     con13,
+     con14,
+     con15,
+     con16,
+     con17,
+     con18,
+     con19,
+     con20
+
+}
