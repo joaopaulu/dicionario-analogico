6ecd8b87c5c43c8ef13131351504ed72e9501d25
João Paulo <jptick@gmail.com>
2021-07-31 17:01:21 -0300

    Update DependencyResource

--- backend/src/main/java/br/com/dicionarioanalogico/resources/DependencyResource.java
+++ backend/src/main/java/br/com/dicionarioanalogico/resources/DependencyResource.java
@@ -1,12 +1,9 @@
 package br.com.dicionarioanalogico.resources;
 
 import br.com.dicionarioanalogico.dto.DependencyDTO;
-import br.com.dicionarioanalogico.dto.VerbetDTO;
 import br.com.dicionarioanalogico.services.DependencyService;
-import br.com.dicionarioanalogico.services.VerbetService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -32,14 +29,14 @@ public class DependencyResource {
     }
 
     @GetMapping(value = "{id}")
-    @ApiOperation("Busca um Verbete por ID")
+    @ApiOperation("Busca um Dependencia por ID")
     public ResponseEntity<DependencyDTO> findById(@PathVariable Long id){
         DependencyDTO dto = service.findById(id);
         return ResponseEntity.ok().body(dto);
     }
 
     @PostMapping
-    @ApiOperation("Insere um novo Verbete")
+    @ApiOperation("Insere um novo Dependencia")
     public ResponseEntity<DependencyDTO> insert(@RequestBody DependencyDTO dto){
         dto = service.insert(dto);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
@@ -48,14 +45,14 @@ public class DependencyResource {
     }
 
     @PutMapping(value = "{id}")
-    @ApiOperation("Atualiza um Verbete")
+    @ApiOperation("Atualiza um Dependencia")
     public ResponseEntity<DependencyDTO> update(@PathVariable Long id, @RequestBody DependencyDTO dto){
         dto = service.update(id, dto);
         return ResponseEntity.ok().body(dto);
     }
 
     @DeleteMapping(value = "{id}")
-    @ApiOperation("Deleta um Verbete")
+    @ApiOperation("Deleta um Dependencia")
     public ResponseEntity<DependencyDTO> delete(@PathVariable Long id){
         service.delete(id);
         return ResponseEntity.noContent().build();
