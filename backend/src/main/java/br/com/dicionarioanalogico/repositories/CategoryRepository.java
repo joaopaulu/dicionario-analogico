8916a7231e4e260999fed5d28cd5ade2ca273535
João Paulo <jptick@gmail.com>
2021-08-31 17:15:19 -0300

    Update Filter Category

--- backend/src/main/java/br/com/dicionarioanalogico/repositories/CategoryRepository.java
+++ backend/src/main/java/br/com/dicionarioanalogico/repositories/CategoryRepository.java
@@ -1,7 +1,13 @@
 package br.com.dicionarioanalogico.repositories;
 
 import br.com.dicionarioanalogico.entities.Category;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    @Query("SELECT DISTINCT obj FROM Category obj WHERE "
+            + "(LOWER(obj.descricao) LIKE LOWER(CONCAT('%',:descricao,'%'))) ")
+    Page<Category> find(String descricao, Pageable pageable);
 }
--- backend/src/main/java/br/com/dicionarioanalogico/resources/CategoryResource.java
+++ backend/src/main/java/br/com/dicionarioanalogico/resources/CategoryResource.java
@@ -7,6 +7,7 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -25,14 +26,9 @@ public class CategoryResource {
     @GetMapping
     @ApiOperation("Consulta todas categoria")
     public ResponseEntity<Page<CategoryDTO>> findAll(
-            @RequestParam(value = "page", defaultValue = "0") Integer page,
-            @RequestParam(value = "linesPerPage", defaultValue = "10") Integer linesPerPage,
-            @RequestParam(value = "direction", defaultValue = "ASC") String direction,
-            @RequestParam(value = "orderBy", defaultValue = "descricao") String orderBy){
-
-        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
-
-        Page<CategoryDTO> list = service.findAllPaged(pageRequest);
+            @RequestParam(value = "descricao", defaultValue = "") String descricao,
+            Pageable pageable) {
+        Page<CategoryDTO> list = service.findAllPaged(descricao.trim(), pageable);
         return ResponseEntity.ok().body(list);
     }
 
