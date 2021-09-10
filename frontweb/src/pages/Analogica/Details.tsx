2c853a948726052b32b7f12952eda948697061e7
João Paulo <jptick@gmail.com>
2021-09-10 09:18:58 -0300

    Update Analogica Details

--- frontweb/src/pages/Analogica/Details.tsx
+++ frontweb/src/pages/Analogica/Details.tsx
@@ -12,7 +12,15 @@ type UrlParams = {
 };
 
 const AnalogicaDetails = () => {
-  const [analogicas, setAnalogicas] = useState<Analogica[]>([]);
+  const [analogicasHip, setAnalogicasHip] = useState<Analogica[]>([]);
+  const [analogicasMer, setAnalogicasMer] = useState<Analogica[]>([]);
+  const [analogicasCon1, setAnalogicasCon1] = useState<Analogica[]>([]);
+  const [analogicasCon2, setAnalogicasCon2] = useState<Analogica[]>([]);
+  const [analogicasCon3, setAnalogicasCon3] = useState<Analogica[]>([]);
+  const [analogicasCon4, setAnalogicasCon4] = useState<Analogica[]>([]);
+  const [analogicasCon5, setAnalogicasCon5] = useState<Analogica[]>([]);
+  const [analogicasCon6, setAnalogicasCon6] = useState<Analogica[]>([]);
+  const [analogicasVer, setAnalogicasVer] = useState<Analogica[]>([]);
   const [tematics, setTematics] = useState<Tematic>();
   const { tematicId } = useParams<UrlParams>();
 
@@ -28,7 +36,71 @@ const AnalogicaDetails = () => {
     requestBackend({
       url: `${BASE_URL}/verbetes/analogicas?dependente=${tematicId}&tpDependencia=1`,
     }).then(response => {
-      setAnalogicas(response.data);
+      setAnalogicasHip(response.data);
+    });
+  }, [tematicId]);
+
+  useEffect(() => {
+    requestBackend({
+      url: `${BASE_URL}/verbetes/analogicas?dependente=${tematicId}&tpDependencia=2`,
+    }).then(response => {
+      setAnalogicasMer(response.data);
+    });
+  }, [tematicId]);
+
+  useEffect(() => {
+    requestBackend({
+      url: `${BASE_URL}/verbetes/analogicas?dependente=${tematicId}&tpDependencia=3`,
+    }).then(response => {
+      setAnalogicasCon1(response.data);
+    });
+  }, [tematicId]);
+
+  useEffect(() => {
+    requestBackend({
+      url: `${BASE_URL}/verbetes/analogicas?dependente=${tematicId}&tpDependencia=4`,
+    }).then(response => {
+      setAnalogicasCon2(response.data);
+    });
+  }, [tematicId]);
+
+  useEffect(() => {
+    requestBackend({
+      url: `${BASE_URL}/verbetes/analogicas?dependente=${tematicId}&tpDependencia=5`,
+    }).then(response => {
+      setAnalogicasCon3(response.data);
+    });
+  }, [tematicId]);
+
+  useEffect(() => {
+    requestBackend({
+      url: `${BASE_URL}/verbetes/analogicas?dependente=${tematicId}&tpDependencia=6`,
+    }).then(response => {
+      setAnalogicasCon4(response.data);
+    });
+  }, [tematicId]);
+
+  useEffect(() => {
+    requestBackend({
+      url: `${BASE_URL}/verbetes/analogicas?dependente=${tematicId}&tpDependencia=7`,
+    }).then(response => {
+      setAnalogicasCon5(response.data);
+    });
+  }, [tematicId]);
+
+  useEffect(() => {
+    requestBackend({
+      url: `${BASE_URL}/verbetes/analogicas?dependente=${tematicId}&tpDependencia=8`,
+    }).then(response => {
+      setAnalogicasCon6(response.data);
+    });
+  }, [tematicId]);
+
+  useEffect(() => {
+    requestBackend({
+      url: `${BASE_URL}/verbetes/analogicas?dependente=${tematicId}&tpDependencia=9`,
+    }).then(response => {
+      setAnalogicasVer(response.data);
     });
   }, [tematicId]);
 
@@ -48,38 +120,168 @@ const AnalogicaDetails = () => {
         </div>
         <div className="container-info">
           <h1>Substantivos</h1>
-          <div className="container-verbets">
-            <span className="title-tipo-dependencia">hipônimo:</span>
-            <span className="txt-verbet">
-              {analogicas?.map(analogica => {
-                return (
-                  <>
-                    <Link to={`/verbete/${analogica.id}`}>
-                      <SubsCard analogica={analogica} key={analogica.id} />
-                    </Link>
-                  </>
-                );
-              })}
-            </span>
-          </div>
-          <div className="container-verbets">
-            <span className="title-tipo-dependencia">merônimo:</span>
-            <span className="txt-verbet">acelerador,</span>
-            <span className="txt-verbet">amortecedor,</span>
-            <span className="txt-verbet">banco,</span>
-            <span className="txt-verbet">buzina,</span>
-            <span className="txt-verbet">cabine,</span>
-            <span className="txt-verbet">capô,</span>
-            <span className="txt-verbet">embreagem,</span>
-            <span className="txt-verbet">escapamento,</span>
-            <span className="txt-verbet">farol,</span>
-            <span className="txt-verbet">freio,</span>
-            <span className="txt-verbet">hélice,</span>
-            <span className="txt-verbet">macaco,</span>
-            <span className="txt-verbet">marchar,</span>
-            <span className="txt-verbet">motor,</span>
-            <span className="txt-verbet">painel</span>
-          </div>
+          {analogicasHip.length > 0 && (
+            <>
+              <div className="container-verbets">
+                <span className="title-tipo-dependencia">hipônimo:</span>
+                <span className="txt-verbet">
+                  {analogicasHip?.map(analogica => {
+                    return (
+                      <>
+                        <Link to={`/verbete/${analogica.id}`}>
+                          <SubsCard analogica={analogica} key={analogica.id} />
+                        </Link>
+                      </>
+                    );
+                  })}
+                </span>
+              </div>
+            </>
+          )}
+          {analogicasMer.length > 0 && (
+            <>
+              <div className="container-verbets">
+                <span className="title-tipo-dependencia">merônimo:</span>
+                <span className="txt-verbet">
+                  {analogicasMer?.map(analogica => {
+                    return (
+                      <>
+                        <Link to={`/verbete/${analogica.id}`}>
+                          <SubsCard analogica={analogica} key={analogica.id} />
+                        </Link>
+                      </>
+                    );
+                  })}
+                </span>
+              </div>
+            </>
+          )}
+          {analogicasCon1.length > 0 && (
+            <>
+              <div className="container-verbets">
+                <span className="title-tipo-dependencia">con1:</span>
+                <span className="txt-verbet">
+                  {analogicasCon1?.map(analogica => {
+                    return (
+                      <>
+                        <Link to={`/verbete/${analogica.id}`}>
+                          <SubsCard analogica={analogica} key={analogica.id} />
+                        </Link>
+                      </>
+                    );
+                  })}
+                </span>
+              </div>
+            </>
+          )}
+          {analogicasCon2.length > 0 && (
+            <>
+              <div className="container-verbets">
+                <span className="title-tipo-dependencia">con2:</span>
+                <span className="txt-verbet">
+                  {analogicasCon2?.map(analogica => {
+                    return (
+                      <>
+                        <Link to={`/verbete/${analogica.id}`}>
+                          <SubsCard analogica={analogica} key={analogica.id} />
+                        </Link>
+                      </>
+                    );
+                  })}
+                </span>
+              </div>
+            </>
+          )}
+          {analogicasCon3.length > 0 && (
+            <>
+              <div className="container-verbets">
+                <span className="title-tipo-dependencia">con3:</span>
+                <span className="txt-verbet">
+                  {analogicasCon3?.map(analogica => {
+                    return (
+                      <>
+                        <Link to={`/verbete/${analogica.id}`}>
+                          <SubsCard analogica={analogica} key={analogica.id} />
+                        </Link>
+                      </>
+                    );
+                  })}
+                </span>
+              </div>
+            </>
+          )}
+          {analogicasCon4.length > 0 && (
+            <>
+              <div className="container-verbets">
+                <span className="title-tipo-dependencia">con4:</span>
+                <span className="txt-verbet">
+                  {analogicasCon4?.map(analogica => {
+                    return (
+                      <>
+                        <Link to={`/verbete/${analogica.id}`}>
+                          <SubsCard analogica={analogica} key={analogica.id} />
+                        </Link>
+                      </>
+                    );
+                  })}
+                </span>
+              </div>
+            </>
+          )}
+          {analogicasCon5.length > 0 && (
+            <>
+              <div className="container-verbets">
+                <span className="title-tipo-dependencia">con5:</span>
+                <span className="txt-verbet">
+                  {analogicasCon5?.map(analogica => {
+                    return (
+                      <>
+                        <Link to={`/verbete/${analogica.id}`}>
+                          <SubsCard analogica={analogica} key={analogica.id} />
+                        </Link>
+                      </>
+                    );
+                  })}
+                </span>
+              </div>
+            </>
+          )}
+          {analogicasCon6.length > 0 && (
+            <>
+              <div className="container-verbets">
+                <span className="title-tipo-dependencia">con6:</span>
+                <span className="txt-verbet">
+                  {analogicasCon6?.map(analogica => {
+                    return (
+                      <>
+                        <Link to={`/verbete/${analogica.id}`}>
+                          <SubsCard analogica={analogica} key={analogica.id} />
+                        </Link>
+                      </>
+                    );
+                  })}
+                </span>
+              </div>
+            </>
+          )}
+          {analogicasVer.length > 0 && (
+            <>
+              <div className="container-verbets">
+                <span className="title-tipo-dependencia">verbo:</span>
+                <span className="txt-verbet">
+                  {analogicasVer?.map(analogica => {
+                    return (
+                      <>
+                        <Link to={`/verbete/${analogica.id}`}>
+                          <SubsCard analogica={analogica} key={analogica.id} />
+                        </Link>
+                      </>
+                    );
+                  })}
+                </span>
+              </div>
+            </>
+          )}
         </div>
       </article>
       <button
