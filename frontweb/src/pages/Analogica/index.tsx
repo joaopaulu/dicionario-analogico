2ec24dec68c416088cd8e855c3bc5409b8586092
João Paulo <jptick@gmail.com>
2021-09-11 14:56:08 -0300

    Styling Parte Analogia With Diagram

--- frontweb/src/pages/Analogica/index.tsx
+++ frontweb/src/pages/Analogica/index.tsx
@@ -3,6 +3,7 @@ import { Tematic } from 'core/types/tematic';
 import { SpringPage } from 'core/types/vendor/spring';
 import { requestBackend } from 'core/utils/requests';
 import { useCallback, useEffect, useState } from 'react';
+import { Tree, TreeNode } from 'react-organizational-chart';
 import { Link } from 'react-router-dom';
 import './styles.scss';
 
@@ -27,24 +28,27 @@ const Analogica = () => {
       <article className="blog-post">
         <h1 className="text-center blog-post-title mt-5 ">Parte Analógica</h1>
       </article>
-      <div>
-        <div className="table-responsive container-tematics">
-          <table className="table table-striped table-sm">
-            <tbody>
-              <>
-                {page?.content?.map(tematic => (
-                  <tr key={tematic.id}>
-                    <td>
-                      <Link to={`/analogica/${tematic.id}`}>
-                        <span className="title-tematics">{tematic.nome}</span>{' '}
-                      </Link>
-                    </td>
-                  </tr>
-                ))}
-              </>
-            </tbody>
-          </table>
-        </div>
+      <div className="tematic-diagram">
+        <Tree
+          label={
+            <div className="tematic-diagram-title">
+              <h3>Campos Temáticos</h3>
+            </div>
+          }
+        >
+          {page?.content?.map(tematic => (
+            <TreeNode
+              label={
+                <Link to={`/analogica/${tematic.id}`}>
+                  <div className="title-tematics">
+                    <span>{tematic.nome}</span>
+                  </div>
+                </Link>
+              }
+              key={tematic.id}
+            />
+          ))}
+        </Tree>
       </div>
     </main>
   );
