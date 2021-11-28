715e6db8be676d09359187796e09d2837c3b4e71
João Paulo <jptick@gmail.com>
2021-11-28 12:47:05 -0300

    Update To Deploy in Tomcat Locaweb

--- backend/src/main/java/br/com/dicionarioanalogico/DicionarioanalogicoApplication.java
+++ backend/src/main/java/br/com/dicionarioanalogico/DicionarioanalogicoApplication.java
@@ -2,9 +2,10 @@ package br.com.dicionarioanalogico;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DicionarioanalogicoApplication {
+public class DicionarioanalogicoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DicionarioanalogicoApplication.class, args);
--- backend/src/main/resources/querys.sql
+++
@@ -1,6 +0,0 @@
-//CONSULTA VERBETES POR CAMPOS TEMATICOS
-SELECT v.descricao, v.separacao_silabica, v.genero, v.definicao, c.nome
-FROM verbetes v
-INNER JOIN campos_tematicos c
-ON v.campo_tematico_id = c.id
-WHERE c.id = 9
